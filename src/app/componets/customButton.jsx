eaf4bf5a78a95eae1d83f61438008c23579c33b1
Joppaa-Main <Joppaaplc@gmail.com>
2023-10-10 04:43:58 +0100

    Revert "first comit"
    This reverts commit d529cfb26dfaf7ea8e8a17a2e1b9e30a7e1426c8.

--- src/app/componets/customButton.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function CustomButton({text,color,textcolor}) {
-    return (<div className={`my-2 border-2 px-6 py-2 ${color} ${textcolor} rounded-2xl`} >
-        <div className="text-base font-bold">{text}</div>
-    </div>)
-}
