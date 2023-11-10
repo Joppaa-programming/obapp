8f3651c2bb3ed41c3e87d95de1858d6ef47539d3
Joppaa-Main <Joppaaplc@gmail.com>
2023-11-10 02:32:05 +0000

    well

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
   title: 'OriginallyBlac',
   description: 'OriginallyBlac',
 }
-
+export const revalidate = 60
 export default function RootLayout({ children }) {
 
   return (
